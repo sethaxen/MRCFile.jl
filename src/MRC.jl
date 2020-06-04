bb5cc31b6e715c75971a4fd87940cc643fd31c2a
Seth Axen <seth.axen@gmail.com>
2020-06-03 21:11:30 -0700

    Add byteorder

--- src/MRC.jl
+++ src/MRC.jl
@@ -5,6 +5,7 @@ export data, header, extended_header, origin, celldims, cellangles, griddims
 
 const HEADER_LENGTH = 1024
 
+include("utils.jl")
 include("header.jl")
 include("extended_header.jl")
 include("data.jl")
---
+++ src/utils.jl
@@ -0,0 +1,1 @@
+byteorder() = ifelse(Base.ENDIAN_BOM == 0x04030201, <, >)
