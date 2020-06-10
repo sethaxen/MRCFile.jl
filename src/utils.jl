3cdfaf9334d8e98cc1f97e687d34a69f5a99798a
Seth Axen <seth.axen@gmail.com>
2020-06-10 00:38:07 -0700

    Refactor smartopen

--- src/utils.jl
+++ src/utils.jl
@@ -15,46 +15,73 @@ function padtruncto!(x, n; value = zero(eltype(x)))
     return x
 end
 
+function checkmagic(io)
+    magic = Base.read(io, 3)
+    seek(io, 0)
+    return if magic == GZ_MAGIC
+        :gz
+    elseif magic == BZ2_MAGIC
+        :bz2
+    else
+        :none
+    end
+end
+
+function checkextension(path)
+    return if endswith(path, ".gz")
+        :gz
+    elseif endswith(path, ".bz2")
+        :bz2
+    else
+        :none
+    end
+end
+
+function compresstream(io, type)
+    return if type == :gz
+        GzipCompressorStream(io)
+    elseif type == :bz2
+        Bzip2CompressorStream(io)
+    elseif type == :none
+        io
+    else
+        throw(IOError("Unrecognized compression type"))
+    end
+end
+
+function decompresstream(io, type)
+    return if type == :gz
+        GzipDecompressorStream(io)
+    elseif type == :bz2
+        Bzip2DecompressorStream(io)
+    elseif type == :none
+        io
+    else
+        throw(IOError("Unrecognized decompression type"))
+    end
+end
+
 """
     smartopen(f::Function, args...; kwargs...)
 
 Open a file and call `f` on the io stream, decompressing if the file appears to be gz or bz2.
 """
 function smartopen(f::Function, args...; read = false, write = false, kwargs...)
-    return if read
+    if read
         io = open(args...; read = read, write = write)
-        magic = Base.read(io, 3)
-        seek(io, 0)
-        if magic == GZ_MAGIC
-            newio = GzipDecompressorStream(io)
-            ret = f(newio)
-            close(newio)
-        elseif magic == BZ2_MAGIC
-            newio = Bzip2DecompressorStream(io)
-            ret = f(newio)
-            close(newio)
-        else
-            ret = f(io)
-        end
+        type = checkmagic(io)
+        newio = decompresstream(io, type)
+        ret = f(newio)
         close(io)
-        ret
     elseif args[1] isa AbstractString && write
-        io = open(args...; read = read, write = write)
         path = args[1]
-        if endswith(path, ".gz")
-            newio = GzipCompressorStream(io)
-            ret = f(newio)
-            close(newio)
-        elseif endswith(path, ".bz2")
-            newio = Bzip2CompressorStream(io)
-            ret = f(newio)
-            close(newio)
-        else
-            ret = f(io)
-        end
+        type = checkextension(path)
+        io = open(args...; read = read, write = write)
+        newio = compresstream(io, type)
+        ret = f(newio)
         close(io)
-        ret
     else
-        open(f, args...; read = read, write = write)
+        ret = open(f, args...; read = read, write = write)
     end
+    return ret
 end
