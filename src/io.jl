d88b20055e6af440d55939f5cb191a8d92ac4ed5
Seth Axen <seth.axen@gmail.com>
2020-06-11 16:59:16 -0700

    Document and reorganize

--- src/io.jl
+++ src/io.jl
@@ -1,14 +1,8 @@
-function Base.read(fn::AbstractString, ::Type{T}; kwargs...) where {T<:MRCData}
-    return open(fn; read = true) do io
-        return read(io, T; kwargs...)
-    end
-end
-
 """
     read(io::IO, ::Type{T}; compress = :auto) where {T<:MRCData}
     read(fn::AbstractString, ::Type{T}; compress = :auto) where {T<:MRCData}
 
-Read an instance of [`MRCData`](@ref) from an io stream or closed file.
+Read an instance of [`MRCData`](@ref) from an IO stream or existing file.
 Use `compress` to specify the decompression with the following options:
 - `:auto`: Infer one of the below compression types from the file
 - `:gz`: GZ
@@ -27,6 +21,11 @@ function Base.read(io::IO, ::Type{T}; compress = :auto) where {T<:MRCData}
     d = MRCData(header, extendedheader)
     read!(newio, d.data)
     return d
+end
+function Base.read(fn::AbstractString, ::Type{T}; compress = :auto) where {T<:MRCData}
+    return open(fn; read = true) do io
+        return read(io, T; compress = compress)
+    end
 end
 
 function Base.read(io::IO, ::Type{T}) where {T<:MRCHeader}
@@ -56,6 +55,31 @@ function Base.read(
     return T(data)
 end
 
+"""
+    write(io::IO, ::MRCData; compress = :none)
+    write(fn::AbstractString, ::MRCData; compress = :auto)
+
+Write an instance of [`MRCData`](@ref) to an IO stream or new file.
+Use `compress` to specify the compression with the following options:
+- `:auto`: Infer one of the below compression types from the file extension
+- `:gz`: GZ
+- `:bz2`: BZ2
+- `:xz`: XZ
+- `:none`: no compression
+"""
+write(::Any, ::MRCData)
+function Base.write(io::IO, d::MRCData; compress = :none)
+    newio = compresstream(io, compress)
+    sz = write(newio, header(d))
+    sz += write(newio, extendedheader(d))
+    T = datatype(header(d))
+    data = parent(d)
+    if T !== eltype(data)
+        data = T.(data)
+    end
+    sz += write(newio, data)
+    return sz
+end
 function Base.write(fn::AbstractString, object::T; compress = :auto) where {T<:Union{MRCData}}
     if compress == :auto
         compress = checkextension(fn)
@@ -75,16 +99,3 @@ function Base.write(io::IO, header::MRCHeader)
 end
 
 Base.write(io::IO, eh::MRCExtendedHeader) = write(io, eh.data)
-
-function Base.write(io::IO, d::MRCData; compress = :none)
-    newio = compresstream(io, compress)
-    sz = write(newio, header(d))
-    sz += write(newio, extendedheader(d))
-    T = datatype(header(d))
-    data = parent(d)
-    if T !== eltype(data)
-        data = T.(data)
-    end
-    sz += write(newio, data)
-    return sz
-end
