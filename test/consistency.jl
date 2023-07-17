3a7a88f8ea7bae18aaf4e6cae05f3a1e74e3454a
Shu Li <zetarylee@gmail.com>
2023-07-17 04:51:01 -0400

    Using CondPkg and PythonCall.

--- test/consistency.jl
+++ test/consistency.jl
@@ -1,9 +1,7 @@
 using Test
-using Conda
-Conda.add("numpy")
-Conda.add("mrcfile")
+using CondaPkg
+using PythonCall
 
-using PyCall
 # Load the Python module/package
 numpy = pyimport("numpy")
 mrcfile = pyimport("mrcfile")
@@ -15,62 +13,62 @@ mrcfile = pyimport("mrcfile")
         # @test map_jl == map_py
     end
 
-    function header_test(header_jl::MRCHeader, header_py::PyObject)
-        @test header_jl.nx == convert(Int32, header_py.nx)
-        @test header_jl.ny == convert(Int32, header_py.ny)
-        @test header_jl.nz == convert(Int32, header_py.nz)
-        @test header_jl.mode == convert(Int32, header_py.mode)
-        @test header_jl.nxstart == convert(Int32, header_py.nxstart)
-        @test header_jl.nystart == convert(Int32, header_py.nystart)
-        @test header_jl.nzstart == convert(Int32, header_py.nzstart)
-        @test header_jl.mx == convert(Int32, header_py.mx)
-        @test header_jl.my == convert(Int32, header_py.my)
-        @test header_jl.mz == convert(Int32, header_py.mz)
-        @test header_jl.cella_x == convert(Float32, header_py.cella.x)
-        @test header_jl.cella_y == convert(Float32, header_py.cella.y)
-        @test header_jl.cella_z == convert(Float32, header_py.cella.z)
-        @test header_jl.cellb_alpha == convert(Float32, header_py.cellb.alpha)
-        @test header_jl.cellb_beta == convert(Float32, header_py.cellb.beta)
-        @test header_jl.cellb_gamma == convert(Float32, header_py.cellb.gamma)
-        @test header_jl.mapc == convert(Int32, header_py.mapc)
-        @test header_jl.mapr == convert(Int32, header_py.mapr)
-        @test header_jl.maps == convert(Int32, header_py.maps)
-        @test header_jl.dmin == convert(Float32, header_py.dmin)
-        @test header_jl.dmax == convert(Float32, header_py.dmax)
-        @test header_jl.dmean == convert(Float32, header_py.dmean)
-        @test header_jl.ispg == convert(Int32, header_py.ispg)
-        @test header_jl.nsymbt == convert(Int32, header_py.nsymbt)
-        @test String([UInt8(b) for b in header_jl.extra1]) == convert(String, header_py.extra1.tobytes())
-        @test header_jl.exttyp == header_py.exttyp.tostring()
-        @test header_jl.nversion == convert(Int32, header_py.nversion)
-        @test String([UInt8(b) for b in header_jl.extra2]) == header_py.extra2.tobytes()
-        @test header_jl.origin_x == convert(Float32, header_py.origin.x)
-        @test header_jl.origin_y == convert(Float32, header_py.origin.y)
-        @test header_jl.origin_z == convert(Float32, header_py.origin.z)
-        @test header_jl.map == header_py.map.tostring()
-        @test collect(header_jl.machst) == convert(Vector{UInt8}, header_py.machst)
-        @test header_jl.rms == convert(Float32, header_py.rms)
-        @test header_jl.nlabl == convert(Int32, header_py.nlabl)
-        @test strip.(collect(header_py.label)) == collect(header_jl.label)
+    function header_test(header_jl::MRCHeader, header_py::Py)
+        @test Bool(header_jl.nx == header_py.nx)
+        @test Bool(header_jl.ny == header_py.ny)
+        @test Bool(header_jl.nz == header_py.nz)
+        @test Bool(header_jl.mode == header_py.mode)
+        @test Bool(header_jl.nxstart == header_py.nxstart)
+        @test Bool(header_jl.nystart == header_py.nystart)
+        @test Bool(header_jl.nzstart == header_py.nzstart)
+        @test Bool(header_jl.mx == header_py.mx)
+        @test Bool(header_jl.my == header_py.my)
+        @test Bool(header_jl.mz == header_py.mz)
+        @test Bool(header_jl.cella_x == header_py.cella.x)
+        @test Bool(header_jl.cella_y == header_py.cella.y)
+        @test Bool(header_jl.cella_z == header_py.cella.z)
+        @test Bool(header_jl.cellb_alpha == header_py.cellb.alpha)
+        @test Bool(header_jl.cellb_beta == header_py.cellb.beta)
+        @test Bool(header_jl.cellb_gamma == header_py.cellb.gamma)
+        @test Bool(header_jl.mapc == header_py.mapc)
+        @test Bool(header_jl.mapr == header_py.mapr)
+        @test Bool(header_jl.maps == header_py.maps)
+        @test Bool(header_jl.dmin == header_py.dmin)
+        @test Bool(header_jl.dmax == header_py.dmax)
+        @test Bool(header_jl.dmean == header_py.dmean)
+        @test Bool(header_jl.ispg == header_py.ispg)
+        @test Bool(header_jl.nsymbt == header_py.nsymbt)
+        @test [UInt8(b) for b in header_jl.extra1] == pyconvert(Vector{UInt8}, header_py.extra1.tobytes())
+        @test [UInt8(b) for b in header_jl.exttyp] == pyconvert(Vector{UInt8}, header_py.exttyp.tobytes())
+        @test Bool(header_jl.nversion == header_py.nversion)
+        @test ([UInt8(b) for b in header_jl.extra2]) == pyconvert(Vector{UInt8}, header_py.extra2.tobytes())
+        @test Bool(header_jl.origin_x == header_py.origin.x)
+        @test Bool(header_jl.origin_y == header_py.origin.y)
+        @test Bool(header_jl.origin_z == header_py.origin.z)
+        @test [UInt8(b) for b in header_jl.map] == pyconvert(Vector{UInt8}, header_py.map.tobytes())
+        @test [UInt8(b) for b in header_jl.machst] == pyconvert(Vector{UInt8}, header_py.machst.tobytes())
+        @test Bool(header_jl.rms == header_py.rms)
+        @test Bool(header_jl.nlabl == header_py.nlabl)
+        @test join(header_jl.label) ==
+            pyconvert(String, header_py.label.tostring().decode().strip())
     end
 
-    function exh_test(exh_jl::MRCExtendedHeader, exh_py::PyObject)
-        @test String([UInt8(b) for b in exh_jl.data]) == exh_py.tobytes()
+    function exh_test(exh_jl::MRCExtendedHeader, exh_py::Py)
+        @test [UInt8(b) for b in exh_jl.data] == pyconvert(Vector{UInt8}, exh_py.tobytes())
     end
 
-    @testset "emd_3001.map" begin
-        emd3001_path = "$(@__DIR__)/testdata/emd_3001.map"
-        emd3001_jl = read("$(@__DIR__)/testdata/emd_3001.map", MRCData)
-        map_jl = emd3001_jl.data
-        header_jl = header(emd3001_jl)
-        exh_jl = extendedheader(emd3001_jl)
+    function compare(map_path::String)
+        emd_jl = read(map_path, MRCData)
+        map_jl = emd_jl.data
+        header_jl = header(emd_jl)
+        exh_jl = extendedheader(emd_jl)
 
-        emd3001_py = mrcfile.open("$(@__DIR__)/testdata/emd_3001.map"; mode="r")
-        data_copy = numpy.copy(emd3001_py.data)  # Create a copy of the data
-        map_py = convert(Array{Float32,3}, data_copy)
-        header_py = emd3001_py.header
-        exh_py = emd3001_py.extended_header
-        emd3001_py.close()  # Make sure the file is closed even if an error occurs
+        emd_py = mrcfile.open(map_path; mode="r")
+        data_copy = numpy.copy(emd_py.data)  # Create a copy of the data
+        map_py = pyconvert(Array{Float32,3}, data_copy)
+        header_py = emd_py.header
+        exh_py = emd_py.extended_header
+        emd_py.close()  # Make sure the file is closed even if an error occurs
 
         @testset "map" begin
             map_test(map_jl, map_py)
@@ -83,5 +81,15 @@ mrcfile = pyimport("mrcfile")
         @testset "extendedheader" begin
             exh_test(exh_jl, exh_py)
         end
+    end
+
+    @testset "emd_3001.map" begin
+        emd3001_path = "$(@__DIR__)/testdata/emd_3001.map"
+        compare(emd3001_path)
+    end
+
+    @testset "emd_3197" begin
+        emd3197_path = "$(@__DIR__)/testdata/emd_3197.map"
+        compare(emd3197_path)
     end
 end
