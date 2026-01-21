580518ec0e104cdaae807fde0380a945076bdf53
Andreas Kröpelin <42342396+andreasKroepelin@users.noreply.github.com>
2026-01-21 15:05:54 +0100

    Apply suggestions from code review
    Co-authored-by: Seth Axen <seth@sethaxen.com>

--- ext/DimensionalDataExt.jl
+++ ext/DimensionalDataExt.jl
@@ -10,11 +10,14 @@ Convert the MRC data into a `DimArray` from DimensionalData.jl.
 
 The `DimArray` has `X`, `Y`, `Z` axes according to the voxel axes stored in the
 header.
-`parent(DimArray(mrc)) == mrc.data` holds.
+
+While the names of the axes may be permuted according to the `mapc`, `mapr`, and `maps` entries in the header, the `DimArray` is always equal to the `MRCData` array.
 """
 function DimArray(mrc::MRCData)
-    axs = voxelaxes(header(mrc))
-    dimaxs = map((D, ax) -> D(ax), (X, Y, Z), axs)
+    h = header(mrc)
+    axs = voxelaxes(h)
+    dimnames = map(Base.Fix1(getindex, (X, Y, Z)), (h.mapc, h.mapr, h.maps))
+    dimaxs = map((D, ax) -> D(ax), dimnames, axs)
     DimArray(mrc.data, dimaxs)
 end
 end
